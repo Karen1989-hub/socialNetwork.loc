f71a25e2aba296843faa9735935dd2e39306b9f7
Karen1989-hub <57526610+Karen1989-hub@users.noreply.github.com>
2019-12-30 01:11:04 +0400

    create autentification

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,9 +49,15 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'firstName' => ['required', 'string', 'max:255'],
+            'lastName' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:6', 'confirmed'],
+            'password' => ['required', 'string', 'min:6'],
+            'month' => ['required'],
+            'year' => ['required'],
+            'gender' => ['required'],
+            'city' => ['required','string', 'max:255'],
+            'country' => ['required','string', 'max:255'],
         ]);
     }
 
@@ -64,9 +70,17 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'firstName' => $data['firstName'],
+            'lastName' => $data['lastName'],            
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'day' => $data['day'],
+            'month' => $data['month'],
+            'year' => $data['year'],
+            'gender' => $data['gender'],
+            'city' => $data['city'],
+            'country' => $data['country'],
+
         ]);
     }
 }
